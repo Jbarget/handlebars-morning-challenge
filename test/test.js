fc1eb6e3f331247a2b1b316505ff1f5f85a97024
Justen <justenbarget@gmail.com>
2016-02-18 10:41:16 +0000

    final commit

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var test = require("tape");
-var handlebars = require("../example/handlebars.js")
+var handlebars = require("../handlebars/handlebars.js")
 
 test("sayHello function prints 'Hello World'", function(t){
   var html = handlebars.sayHello();
@@ -11,13 +11,13 @@ test("sayHello function prints 'Hello World'", function(t){
 test("listOutPeople function prints a list of the best people in FAC", function(t){
   var html = handlebars.listOutPeople();
   console.log(html);
-  t.ok(html.indexOf("<li>Dan Sofer</li> <li>Ines Teles</li> <li>Nelson Correira</li>"), "YAY! thats correct!");
+  t.ok(html.indexOf("<li>Dan Sofer</li> <li>Ines Teles</li> <li>Nelson Correira</li>") > -1, "YAY! thats correct!");
   t.end()
 })
 
 test("getInContact function prints FAC's contact details", function(t){
   var html = handlebars.getInContact();
   console.log(html);
-  t.ok(html.indexOf("Please email hello@foundersandcoders.com if you want to get in touch!"), "Congrats! You printed out the email!");
+  t.ok(html.indexOf("Please email hello@foundersandcoders.com if you want to get in touch!") > -1, "Congrats! You printed out the email!");
   t.end();
 })
