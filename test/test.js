4e8a258458480fbf642ea10c6eb2c21237f0ea55
Justen <justenbarget@gmail.com>
2016-11-20 17:25:02 +0000

    adjusting test 3 to require registering a helper

--- test/test.js
+++ test/test.js
@@ -10,14 +10,13 @@ test("sayHello function prints 'Hello World'", function(t){
 
 test("listOutPeople function prints a list of the best people in FAC", function(t){
   var html = handlebars.listOutPeople();
-  console.log(html);
-  t.ok(html.indexOf("<li>Dan Sofer</li> <li>Ines Teles</li> <li>Nelson Correira</li>") > -1, "YAY! thats correct!");
+  var pattern = new RegExp(/<li>Nelson Correira<\/li>\s?<li>Dan Correira<\/li>\s?<li>Ines Correira<\/li>/);
+  t.ok(html.search(/<li>Dan Sofer<\/li>\s{0,}<li>Ines Teles<\/li>\s{0,}<li>Nelson Correira<\/li>/) > -1, "YAY! thats correct!");
   t.end()
 })
 
 test("getInContact function prints FAC's contact details", function(t){
   var html = handlebars.getInContact();
-  console.log(html);
-  t.ok(html.indexOf("Please email hello@foundersandcoders.com if you want to get in touch!") > -1, "Congrats! You printed out the email!");
+  t.ok(html.indexOf("Please email hello@foundersandcoders.com or call +44 208 3583 2442 if you want to get in touch!") > -1, "Congrats! You printed out the email and formatted phone number!");
   t.end();
 })
