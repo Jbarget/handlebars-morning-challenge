65ad302ee79e9ae10b67ca1335b3646c0a9c9b24
jbarget <dear.justen@gmail.com>
2017-02-26 12:45:48 +0200

    adding solution with custom helper and if statments

--- handlebars/handlebars.js
+++ handlebars/handlebars.js
@@ -4,12 +4,18 @@ var fs = require('fs');
 var source = fs.readFileSync(__dirname + "/message.html")
 var template = Handlebars.compile(source.toString());
 
+Handlebars.registerHelper("formatPhoneNumber", function(phoneNumber) {
+  phoneNumber = phoneNumber.toString();
+  return "+44 " + phoneNumber.substr(1,3) + " " + phoneNumber.substr(4,3) + " " + phoneNumber.substr(7,4);
+});
+
 function sayHello(){
   var obj = {
     title:"Hello World",
     post: "A bit of info about FAC!"
   }
   var html = template(obj);
+  console.log(html);
   return html;
 }
 
