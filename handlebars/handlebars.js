a3942c7c9ea4aec5feb2de91665f7d362fa98421
jbarget <dear.justen@gmail.com>
2017-04-03 15:54:13 +0100

    adding branch to show best-practice/DRY code. #16

--- handlebars/handlebars.js
+++ handlebars/handlebars.js
@@ -4,43 +4,27 @@ var fs = require('fs');
 var source = fs.readFileSync(__dirname + "/message.hbs") // need to read the template from the file system
 var template = Handlebars.compile(source.toString()); // need to call Handlebars.compile with the template file
 
-// in each of the functions below you will see `var html = template(context)`
-// This merges the template and the relevant data together to produce the html
-
 Handlebars.registerHelper("formatPhoneNumber", function(phoneNumber) {
   phoneNumber = phoneNumber.toString();
   return "+44 " + phoneNumber.substr(1,3) + " " + phoneNumber.substr(4,3) + " " + phoneNumber.substr(7,4);
 });
 
-function sayHello(){
+// In the function below you will see `var html = template(context)`
+// This merges the template and the relevant data together to produce the html
+
+function renderHtml(){
   var context = {
     title:"Hello World",
-    post: "A bit of info about FAC!"
-  }
-
-  var html = template(context);
-  return html;
-}
-
-function listOutPeople(){
-  var context = {
-    people: ["Dan Sofer", "Ines Teles", "Nelson Correira"]
-  }
-  var html = template(context);
-  return html;
-}
-
-function getInContact(){
-  var context = {
+    post: "A bit of info about FAC!",
+    people: ["Dan Sofer", "Ines Teles", "Nelson Correira"],
     email: "hello@foundersandcoders.com",
     number: '02035832442'
   }
+
   var html = template(context);
   return html;
 }
 
 module.exports = {
-  sayHello: sayHello,
-  listOutPeople: listOutPeople,
-  getInContact: getInContact
+  renderHtml: renderHtml
 }
--- test/test.js
+++ test/test.js
@@ -1,23 +1,13 @@
 var test = require("tape");
 var handlebars = require("../handlebars/handlebars.js")
 
-test("sayHello function prints 'Hello World'", function(t){
-  t.plan(1);
-  var html = handlebars.sayHello();
+test("renders correct html from context object", function(t){
+  t.plan(3);
+  var html = handlebars.renderHtml();
+
   t.ok(html.indexOf("Hello World: A bit of info about FAC!") > -1, "html should contain 'Hello World: A bit of info about FAC!'");
-  t.end();
-})
-
-test("listOutPeople function prints a list of the best people in FAC", function(t){
-  t.plan(1);
-  var html = handlebars.listOutPeople();
   t.ok(html.search(/<li>Dan Sofer<\/li>\s{0,}<li>Ines Teles<\/li>\s{0,}<li>Nelson Correira<\/li>/) > -1, "html should contain 3 li elements each with Dan Sofer, Ines Teles and Nelson Correira ");
-  t.end();
-})
-
-test("getInContact function prints FAC's contact details", function(t){
-  t.plan(1);
-  var html = handlebars.getInContact();
   t.ok(html.indexOf("Please email hello@foundersandcoders.com or call +44 203 583 2442 if you want to get in touch!") > -1, "html should contain the contact info for founders and coders");
+
   t.end();
-})
+});
