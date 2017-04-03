381c3308cd95a53303e31022bb3eb18ab0691e11
Justen Barget <Jbarget@users.noreply.github.com>
2017-04-03 15:33:54 +0100

    updating test messages to better describe error.  #17

--- test/test.js
+++ test/test.js
@@ -4,20 +4,20 @@ var handlebars = require("../handlebars/handlebars.js")
 test("sayHello function prints 'Hello World'", function(t){
   t.plan(1);
   var html = handlebars.sayHello();
-  t.ok(html.indexOf("Hello World: A bit of info about FAC!") > -1, "Congrats! Good introduction!");
+  t.ok(html.indexOf("Hello World: A bit of info about FAC!") > -1, "html should contain 'Hello World: A bit of info about FAC!'");
   t.end();
 })
 
 test("listOutPeople function prints a list of the best people in FAC", function(t){
   t.plan(1);
   var html = handlebars.listOutPeople();
-  t.ok(html.search(/<li>Dan Sofer<\/li>\s{0,}<li>Ines Teles<\/li>\s{0,}<li>Nelson Correira<\/li>/) > -1, "YAY! thats correct!");
+  t.ok(html.search(/<li>Dan Sofer<\/li>\s{0,}<li>Ines Teles<\/li>\s{0,}<li>Nelson Correira<\/li>/) > -1, "html should contain 3 li elements each with Dan Sofer, Ines Teles and Nelson Correira ");
   t.end();
 })
 
 test("getInContact function prints FAC's contact details", function(t){
   t.plan(1);
   var html = handlebars.getInContact();
-  t.ok(html.indexOf("Please email hello@foundersandcoders.com or call +44 203 583 2442 if you want to get in touch!") > -1, "Congrats! You printed out the email and formatted phone number!");
+  t.ok(html.indexOf("Please email hello@foundersandcoders.com or call +44 203 583 2442 if you want to get in touch!") > -1, "html should contain the contact info for founders and coders");
   t.end();
 })
